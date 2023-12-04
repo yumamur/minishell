7bf01d8b256f8f6b5f1a3cd2ab99164924ae3d25
Taha Çelik <114019192+tatataha@users.noreply.github.com>
2023-12-04 19:42:28 +0300

    parameters fixed

--- builtin_parse.c
+++ builtin_parse.c
@@ -4,7 +4,7 @@
 int	builtin_parser(t_cmd builtin)
 {
 	if (ft_strcmp(builtin.cmd, "cd") == 0 && ft_strlen(builtin.cmd) == 2)
-		ft_cd(builtin.args);
+		ft_cd(builtin.args[0]);
 	else if (ft_strcmp(builtin.cmd, "echo") == 0 && ft_strlen(builtin.cmd) == 4)
 		ft_echo(builtin.args);
 	else if (ft_strcmp(builtin.cmd, "unset") == 0 && ft_strlen(builtin.cmd) == 5)
@@ -12,11 +12,11 @@ int	builtin_parser(t_cmd builtin)
 	else if (ft_strcmp(builtin.cmd, "export") == 0 && ft_strlen(builtin.cmd) == 6)
 		ft_export(builtin.args);
 	else if (ft_strcmp(builtin.cmd, "pwd") == 0 && ft_strlen(builtin.cmd) == 3)
-		ft_pwd(builtin.args);
+		ft_pwd();
 	else if (ft_strcmp(builtin.cmd, "env") == 0 && ft_strlen(builtin.cmd) == 3)
-		ft_env(builtin.args);
+		ft_env();
 	else if (ft_strcmp(builtin.cmd, "exit") == 0 && ft_strlen(builtin.cmd) == 4)
-		ft_exit(builtin.args);
+		ft_exit(builtin.args, last_cmd_exit;);
 	else
 		return (1);
 	return (0);
