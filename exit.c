41b4e826cbc31efa80e8dbc30350ac77902f0ad4
Taha Çelik <114019192+tatataha@users.noreply.github.com>
2023-12-04 18:19:15 +0300

    fixed, last_exit parameter added

--- exit.c
+++ exit.c
@@ -1,11 +1,10 @@
 #include "builtin.h"
 #include <limits.h>
 
-int	ret_for_exit = 0; //msh_struct tan düzenlenecek
 void	ft_free_2pt(char **arr);
 int	errorer(char *command, char *detail, char *error_message, int error_nb);
 
-int	ft_exit_atoi(char *arg)
+int	ft_exit_atoi(char *arg, int *last_exit)
 {
 	int			i;
 	long		neg;
@@ -25,23 +24,23 @@ int	ft_exit_atoi(char *arg)
 		num = num * 10 + (arg[i] - 48);
 		if (((i == 18 && neg == 1) && (arg[i] > '7' && arg[i] <= '9'))
 			|| ((i == 19 && neg == -1) && (arg[i] == '9')))
-			ret_for_exit = 2;
+			last_exit = 2;
 		i++;
 	}
 	return (num * neg);
 }
 
-void	exit_door(char *arg)
+void	exit_door(char *arg, int *last_exit)
 {
 	long long	errnum;
 
-	errnum = ft_exit_atoi(arg);
+	errnum = ft_exit_atoi(arg, last_exit);
 	if (!(errnum < LLONG_MAX && errnum > LLONG_MIN))
 	{
 		free(arg);
 		exit(-1);
 	}
-	ret_for_exit = errnum % 256;
+	last_exit = errnum % 256;
 }
 
 int	numeric_ctrl(char *arg)
@@ -60,7 +59,7 @@ int	numeric_ctrl(char *arg)
 	return (0);
 }
 
-int	ft_exit(char **arg)//int *last_exit, global kullanmiyoruz buralarda
+int	ft_exit(char **arg, int *last_exit)
 {
 	if (!arg[1])
 	{
@@ -75,6 +74,6 @@ int	ft_exit(char **arg)//int *last_exit, global kullanmiyoruz buralarda
 	if (arg[2])
 		return(errorer("exit", "too many argument for exit", NULL, EXIT_FAILURE));
 	else
-		exit_door(arg[1]);
-	exit(ret_for_exit);
+		exit_door(arg[1], last_exit);
+	exit(last_exit);
 }
