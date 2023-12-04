5c1982973fc49916ca7d44342195b163324a94ce
Taha Çelik <114019192+tatataha@users.noreply.github.com>
2023-12-04 19:14:17 +0300

    prototypes revised

--- builtin.h
+++ builtin.h
@@ -10,7 +10,7 @@ int	ft_cd(char **env, char **arg);
 int	ft_echo(char **arg);
 int	ft_exit(char **arg, int *last_exit);
 void ft_export(char **arg);
-int	ft_pwd(char **env, char **arg);
+int	ft_pwd(void);
 int	ft_unset(char **arg);
 int	ft_env(void);
 
