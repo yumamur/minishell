696a4cffdc2767f0b1a79cc1fe3df214c60108b1
Taha Çelik <114019192+tatataha@users.noreply.github.com>
2023-12-04 14:44:26 +0300

    parse.c/.h & utils uploaded

--- lexer.c
+++ lexer.c
@@ -1,100 +1,76 @@
-#include <stdlib.h>
-#include "libft/libft.h"
+#include "parse.h"
+#define NULL 0
 
-int	control(char *str)
+t_c_char **parse_cmd(t_c_char *cmd, int a, t_parse_arg arg);
+
+void	add_node(t_tokenzied_list **head, t_c_char *cmd, t_token token)
 {
-	if (*str == '\"' && ft_strchr(str, '\"'))
+	t_tokenzied_list	*tmp;
+	t_tokenzied_list	*new_node;
+    t_parse_arg	arg;
+    
+    if (token == 0 && is_seperator(cmd) != 0)
+    {
+		arg.parsed = (t_c_char **)malloc(sizeof(t_c_char *) * 24);
+        arg.parsed = parse_cmd(cmd, 0, arg); // *cmd = "taha|cat>1" -> **seperate = "taha" "|" "cat" ">" "1"
+		//print_d(arg.parsed);
+        lexer(arg.parsed, head);
+		//ft_free_2pt((char **)arg.parsed);
+		//print_d(arg.parsed);
+		return ;
+    }
+    new_node = (t_tokenzied_list*)malloc(sizeof(t_tokenzied_list));
+	if (!new_node)
+        return ;
+	new_node->str = cmd;
+	new_node->token = token;
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else
 	{
-		while (*++str && *str != '\"')
-		{
-			if (*str == '\t')
-				*str = -2;
-			else if (*str == ' ')
-				*str = -3;
-		}
-		return (1);
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
 	}
-	else if (*str == '\'' && ft_strchr(str, '\''))
-	{
-		while (*++str && *str != '\'')
-		{
-			if (*str == '\t')
-				*str = -2;
-			else if (*str == ' ')
-				*str = -3;
-		}
-		return (1);
-	}
-	return (0);
+
 }
 
-static int	count_words(char *str)
+t_tokenzied_list	**lexer(t_c_char **words, t_tokenzied_list **for_tok)
 {
-	int	ret;
-
-	ret = 0;
-	while (*str)
+	t_tokenzied_list **tokended;
+	if (!for_tok)
 	{
-		while (*str == '\t' || *str == ' ')
-			++str;
-		if (*str == '\"' || *str == '\'')
-		{
-			if (control(str) == 1)
-			{
-				ret++;
-				str = ft_strchr(str + 1, *str) + 1;
-			}
-			else
-				return (0);
-		}
-		if (*str)
-			++ret;
-		while (*str && *str != '\t' && *str != ' '
-			&& *str != '\"' && *str != ' ')
-			++str;
+		tokended = malloc(sizeof(t_tokenzied_list));
+		if (!tokended)
+			return(0);
+		*tokended = NULL;
 	}
-	return (ret);
-}
-
-static void	fixer(char	**str)
-{
+	else
+		tokended = for_tok;
 	int	i;
 
-	while (*str)
+	i = 0;
+	while(words[i])
 	{
-		i = 0;
-		while ((*str)[i])
-		{
-			if ((*str)[i] == -2)
-				(*str)[i] = '\t';
-			else if ((*str)[i] == -3)
-				(*str)[i] = ' ';
-			i++;
-		}
-		str++;
+		if (words[i][0] == '|' && ft_strlen(words[i]) == 1)
+			add_node(tokended, words[i], PIPE);
+		else if (words[i][0] == '<' && ft_strlen(words[i]) == 1)
+			add_node(tokended, words[i], REDIRECT_IN);
+		else if (words[i][0] == '>' && ft_strlen(words[i]) == 1)
+			add_node(tokended, words[i], REDIRECT_OUT);
+		else if ((words[i][0] == '>') && words[i][1] == '>' && ft_strlen(words[i]) == 2)
+			add_node(tokended, words[i], APPEND);
+		else if (words[i][0] == '<' && words[i][1] == '<' && ft_strlen(words[i]) == 2)
+			add_node(tokended, words[i], HEREDOC);
+		else if (words[i][0] == '|' && words[i][1] == '|' && ft_strlen(words[i]) == 2)
+			add_node(tokended, words[i], OR);
+		else if (words[i][0] == '&' && words[i][1] == '&' && ft_strlen(words[i]) == 2)
+			add_node(tokended, words[i], AND);
+		else
+			add_node(tokended, words[i], WORD);
+		i++;
 	}
-}
-
-char	**ft_str_wordtab(char *str)
-{
-	char	**ret;
-	int		ct_word;
-
-	ct_word = count_words(str);
-	ret = malloc(++ct_word * sizeof(char *));
-	if (!ret)
-		return (0);
-	ct_word = 0;
-	while (*str)
-	{
-		while ((*str == '\t' || *str == ' '))
-			*str++ = 0;
-		if (*str)
-			ret[ct_word++] = str;
-		while (*str && *str != '\t' && *str != ' ')
-			++str;
-	}
-	ret[ct_word] = 0;
-	fixer(ret);
-	return (ret);
+	return(tokended);
 }
