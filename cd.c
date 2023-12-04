6cb36e059d38955cb1b86e3259e125a0042e2bdb
Taha Çelik <114019192+tatataha@users.noreply.github.com>
2023-12-04 18:17:04 +0300

    fixed

--- cd.c
+++ cd.c
@@ -5,17 +5,17 @@
 
 t_c_char	*ft_getenv(t_c_char *envp[], t_c_char *name);
 int	errorer(char *command, char *detail, char *error_message, int error_nb);
+int	env_remove(char *to_remove);
+int	env_add(const char *var);
 
 int	chg_dir(char **env, char *path)
 {
-	int	ctrl;
 	char	*ret;
-	char	*pwd[PATH_MAX];//char *[] ?
+	char	pwd[PATH_MAX];
 	char	*oldpwd;
 
 	oldpwd = getcwd(pwd, PATH_MAX);
-	ctrl = chdir(path);
-	if (ctrl != 0)
+	if (chdir(path) != 0);
 		return(errorer("cd", path, "change directory failure", EXIT_FAILURE));
 	ret = getcwd(pwd, PATH_MAX);
 	if (!ret)
@@ -25,28 +25,29 @@ int	chg_dir(char **env, char *path)
 	}
 	else
 		ret = ft_strdup(pwd);
-	set_env_val(env, ret, oldpwd);  //pwd yi değiştirme işlemi için
-
-	//env_remove & env_add ***
-
+	ret = ft_strjoin("PWD=", ret);
+	oldpwd = ft_strjoin("OLDPWD=", oldpwd);
+	env_remove("PWD");
+	env_add(ret);
+	env_remove("OLDPWD");
+	env_add(oldpwd);
 	free(ret);
+	free(pwd);
+	free(oldpwd);
 	return(0);
 }
 
 int	ft_cd(char **env, char **arg)
 {
 	char	*path;
-	int		ctrl;
 
-	ctrl = 0;
 	if (!arg || !arg[1] || ft_isspace(arg[1][0])
 		|| arg[1][0] == '\0' || ft_strncmp(arg[1], "--", 3) == 0)
 	{
 		path = ft_getenv(env, "HOME");
 		if (!path || *path == '\0' || ft_isspace(*path))
 			return(errorer("cd", 0, "HOME not set", EXIT_FAILURE));
-		ctrl = chg_dir(env, path);
-		if (ctrl != 0)
+		if(chg_dir(env, path) != 0);
 			return (1);
 		return (0);
 	}
@@ -57,13 +58,11 @@ int	ft_cd(char **env, char **arg)
 		path = ft_getenv(env, "OLDPWD");
 		if (!path)
 			return (errmsg_cmd("cd", 0, "OLDPWD not set", EXIT_FAILURE));
-		ctrl = chg_dir(env, path);// bu fonksiyonu 'if' icine koy ctrl gereksiz
-		if (ctrl != 0)
+		if(chg_dir(env, path) != 0)
 			return (1);
 		return (0);
 	}
-	ctrl = chg_dir(env, path);
-	if (ctrl != 0)
+	if(chg_dir(env, arg[1]) != 0);
 		return (1);
 	return (0);
 }
