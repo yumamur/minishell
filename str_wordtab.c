a3a80fd53cafb8b15138c99574d5cd5fb127e654
tatataha <taha24cel@gmail.com>
2023-12-06 17:21:10 +0300

    quote error

--- str_wordtab.c
+++ str_wordtab.c
@@ -1,29 +1,46 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/resource.h>
 #include "libft/libft.h"
 
 int	control(char *str)
 {
-	if (*str == '\"' && ft_strchr(str, '\"'))
+	int i = 0;
+	while (str[i])
 	{
-		while (*++str && *str != '\"')
+		if (str[i] == '\'')
 		{
-			if (*str == '\t')
-				*str = -2;
-			else if (*str == ' ')
-				*str = -3;
+			(i)++;
+			while (str[i] && str[i] != '\'')
+			{
+				(i)++;
+				if (str[i] == '\t')
+					str[i] = -2;
+				else if (str[i] == ' ')
+					str[i] = -3;
+			}
+			if (str[i] == '\'' && (i)++)
+				return (1);
+			else
+				return (0);
 		}
-		return (1);
-	}
-	else if (*str == '\'' && ft_strchr(str, '\''))
-	{
-		while (*++str && *str != '\'')
+		else if (str[i] == '"')
 		{
-			if (*str == '\t')
-				*str = -2;
-			else if (*str == ' ')
-				*str = -3;
+			(i)++;
+			while (str[i] && str[i] != '"')
+			{
+				(i)++;
+				if (str[i] == '\t')
+					str[i] = -2;
+				else if (str[i] == ' ')
+					str[i] = -3;
+			}
+			if (str[i] == '"' && (i)++)
+				return (1);
+			else
+				return (0);
 		}
-		return (1);
+		break ;
 	}
 	return (0);
 }
@@ -31,21 +48,27 @@ int	control(char *str)
 static int	count_words(char *str)
 {
 	int	ret;
+	int a = 0;
 
 	ret = 0;
 	while (*str)
 	{
+		printf("%s %d\n", str, a++);
 		while (*str == '\t' || *str == ' ')
 			++str;
 		if (*str == '\"' || *str == '\'')
 		{
 			if (control(str) == 1)
 			{
+				printf("kalem : \n");
 				ret++;
 				str = ft_strchr(str + 1, *str) + 1;
 			}
 			else
+			{
+				printf("elma armut\n");
 				return (0);
+			}
 		}
 		if (*str)
 			++ret;
@@ -81,6 +104,8 @@ char	**ft_str_wordtab(char *str)
 	int		ct_word;
 
 	ct_word = count_words(str);
+	if (ct_word == 0)
+		return (0);
 	ret = malloc(++ct_word * sizeof(char *));
 	if (!ret)
 		return (0);
