d8034028368e8a8355bd659d1672040bed342013
tatataha <taha24cel@gmail.com>
2023-12-08 20:12:57 +0300

    lexer fix

--- lexer.c
+++ lexer.c
@@ -1,28 +1,33 @@
+#include "libft/libft.h"
+#include "liblpc/typeft.h"
 #include "parse.h"
 #include "typeft.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void	add_node(t_tokenzied_list **head, t_c_char *cmd, t_token token)
+int	lpc_export(void *addr, ...);
+int	lpc_flush(void);
+
+void	free_tokenized_list(t_tokenzied_list *head)
 {
 	t_tokenzied_list	*tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free((char *)tmp->str);
+		free(tmp);
+	}
+}
+
+void	add_node(t_tokenzied_list **head, t_c_char *cmd, t_token token)
+{
 	t_tokenzied_list	*new_node;
-    t_c_char 			**arg;
-    
-    if (token == 0 && is_seperator(cmd) != 0)
-    {
-		arg = (t_c_char **)malloc(sizeof(t_c_char *) * 10000);
-        arg = parse_cmd(cmd, 0, arg); // *cmd = "taha|cat>1" -> **seperate = "taha" "|" "cat" ">" "1"
-	//	printf("elma : %s\n", arg[0]);
-		//print_d(arg);
-        lexer(arg, head);
-		//ft_free_2pt((char **)arg);
-		//print_d(arg);
-		return ;
-    }
-    new_node = (t_tokenzied_list*)malloc(sizeof(t_tokenzied_list));
+
+	new_node = malloc(sizeof(t_tokenzied_list));
 	if (!new_node)
-        return ;
+		return ;
 	new_node->str = cmd;
 	new_node->token = token;
 	new_node->next = NULL;
@@ -30,50 +35,40 @@ void	add_node(t_tokenzied_list **head, t_c_char *cmd, t_token token)
 		*head = new_node;
 	else
 	{
-		tmp = *head;
+		t_tokenzied_list	*tmp = *head;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = new_node;
 	}
-
+	lpc_export(new_node, free_tokenized_list, NULL);
 }
 
-t_tokenzied_list	**lexer(t_c_char **words, t_tokenzied_list **for_tok)
+t_tokenzied_list	*lexer(t_c_char **words)
 {
-	t_tokenzied_list **tokended;
-	if (!words)
-		return NULL;
-	if (!for_tok)
-	{
-		tokended = malloc(sizeof(t_tokenzied_list));
-		if (!tokended)
-			return(0);
-		*tokended = NULL;
-	}
-	else
-		tokended = for_tok;
+	t_tokenzied_list *tokended = NULL;
 	int	i;
 
 	i = 0;
 	while(words[i])
 	{
 		if (words[i][0] == '|' && ft_strlen(words[i]) == 1)
-			add_node(tokended, words[i], PIPE);
+			add_node(&tokended, words[i], PIPE);
 		else if (words[i][0] == '<' && ft_strlen(words[i]) == 1)
-			add_node(tokended, words[i], REDIRECT_IN);
+			add_node(&tokended, words[i], REDIRECT_IN);
 		else if (words[i][0] == '>' && ft_strlen(words[i]) == 1)
-			add_node(tokended, words[i], REDIRECT_OUT);
+			add_node(&tokended, words[i], REDIRECT_OUT);
 		else if ((words[i][0] == '>') && words[i][1] == '>' && ft_strlen(words[i]) == 2)
-			add_node(tokended, words[i], APPEND);
+			add_node(&tokended, words[i], APPEND);
 		else if (words[i][0] == '<' && words[i][1] == '<' && ft_strlen(words[i]) == 2)
-			add_node(tokended, words[i], HEREDOC);
+			add_node(&tokended, words[i], HEREDOC);
 		else if (words[i][0] == '|' && words[i][1] == '|' && ft_strlen(words[i]) == 2)
-			add_node(tokended, words[i], OR);
+			add_node(&tokended, words[i], OR);
 		else if (words[i][0] == '&' && words[i][1] == '&' && ft_strlen(words[i]) == 2)
-			add_node(tokended, words[i], AND);
+			add_node(&tokended, words[i], AND);
 		else
-			add_node(tokended, words[i], WORD);
+			add_node(&tokended, words[i], WORD);
 		i++;
 	}
-	return(tokended);
+	lpc_flush();
+	return (tokended);
 }
--- parse.h
+++ parse.h
@@ -5,9 +5,9 @@
 #include "libft/libft.h"//kullanilmamis
 #include "msh_structs.h"//kullanilmamis
 
-t_c_char **parse_cmd(t_c_char *cmd, int a, t_c_char **arg);
-t_tokenzied_list	**lexer(t_c_char **words, t_tokenzied_list **for_tok);
+t_c_char            **parse_cmd(t_c_char *cmd, int a, t_c_char **arg);
+t_tokenzied_list	*lexer(t_c_char **words);
 int 				is_seperator(t_c_char *strr);
-char	**ft_str_wordtab(char *str);
+char	            **ft_str_wordtab(char *str);
 
 #endif
